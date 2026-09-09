8893de153247844a05bb3263d8b44263bab87401
Prasanna350 <pashadeviprasanna@gmail.com>
2026-09-09 16:00:05 +0530

    Add second line output to App.java

--- src/main/java/devops/App.java
+++ src/main/java/devops/App.java
@@ -6,5 +6,6 @@ package devops;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This is second line");
     }
 }
